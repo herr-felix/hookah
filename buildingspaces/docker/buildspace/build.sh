c5f7304707f6e4fefe0e124a205d724ebc25580c
Felix Beaulieu <3952025+herr-felix@users.noreply.github.com>
2019-02-12 21:56:15 -0500

    Quicker demo build

--- buildingspaces/docker/buildspace/build.sh
+++ buildingspaces/docker/buildspace/build.sh
@@ -1,20 +1,22 @@
-git version
-
 build() {
-  PROJECT_DIR=$( /bin/sh /opt/handlers/pull/$PULL_HANDLER.sh | tail -1 )
+  PROJECT_DIR=$( /bin/sh /opt/handlers/pull/$PULL_HANDLER.sh $PULL_PARAMS | tail -1 )
   echo "Moving to $PROJECT_DIR"
   cd $PROJECT_DIR
+  if [ "$?" -ne "0" ]; then
+    echo "Can't move to project '$PROJECT_DIR'"
+    return 1
+  fi
 
   echo "Moving to $BUILD_PATH"
   cd $BUILD_PATH
   if [ "$?" -ne "0" ]; then
-    echo "Moving to '$BUILD_PATH' failed"
+    echo "Moving to build path '$BUILD_PATH'"
     return 1
   fi
 
   make all
   if [ "$?" -ne "0" ]; then
-    echo "Build fail"
+    echo "Build failed"
     return 1
   fi
 
